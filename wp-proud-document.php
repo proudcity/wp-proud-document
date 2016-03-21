1dddc3611b35f5c796e22cfc91ba73aaee42c528
Jeff Lyon <jeff@albatrossdigital.com>
2016-03-20 22:17:49 -0700

    Adding prototype proud heart widget to documents #240

--- wp-proud-document.php
+++ wp-proud-document.php
@@ -6,7 +6,7 @@ Description: Declares an Document custom post type.
 Version: 1.0
 Author: ProudCity
 Author URI: http://proudcity.com/
-License: GPLv2
+License: Affero GPL v3
 */
 
 namespace Proud\Document;
